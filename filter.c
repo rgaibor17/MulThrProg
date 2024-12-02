bb7dd8dc93f6f6d66de03932a10d1ec425ec0cfd
rgaibor17 <rgaibor@espol.edu.ec>
2024-12-01 21:33:24 -0500

    Main filter function

--- filter.c
+++ filter.c
@@ -147,3 +147,51 @@ void applyParallel(BMP_Image *imageIn, BMP_Image *imageOut, int boxFilter[3][3],
     free(threads);
     free(threadArgs);
 }
+
+int main(int argc, char **argv) {
+    FILE* source;
+    FILE* dest;
+    BMP_Image* imageIn = NULL;
+    BMP_Image* imageOut = NULL;
+    int numThreads = 4;
+    int boxFilter[3][3] = {
+        {1, 1, 1},
+        {1, 1, 1},
+        {1, 1, 1}
+    };
+
+    if (argc != 3) {
+        printError(ARGUMENT_ERROR);
+        exit(EXIT_FAILURE);
+    }
+    
+    if((source = fopen(argv[1], "rb")) == NULL) {
+        printError(FILE_ERROR);
+        exit(EXIT_FAILURE);
+    }
+    if((dest = fopen(argv[2], "wb")) == NULL) {
+        printError(FILE_ERROR);
+        exit(EXIT_FAILURE);
+    }
+
+    readImage(source, imageIn);
+    if(!checkBMPValid(&imageIn->header)) {
+        printError(VALID_ERROR);
+        exit(EXIT_FAILURE);
+    }
+
+    imageOut = createBMPImage(dest);
+    applyParallel(imageIn, imageOut, boxFilter, numThreads);
+
+    readImage(source, imageOut);
+    printBMPHeader(&imageOut->header);
+    printBMPImage(imageOut);
+    writeImage("destination.bmp", imageOut);
+
+    freeImage(imageIn);
+    freeImage(imageOut);
+    fclose(source);
+    fclose(dest);
+
+    exit(EXIT_SUCCESS);
+}
